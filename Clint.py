f0b4f51082de8df676ca419b237a47f91a0c566f
ZiyadZM <ZiyadZabin@gmail>
2023-09-04 17:55:18 +0300

    commit some changes

--- Clint.py
+++ Clint.py
@@ -1,6 +1,12 @@
 import socket
-s = socket.socket()
-port = 56988
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+port = 56788
 s.connect(("192.168.100.147", port))
+
+client_message = "Hello, server!"
+s.send(client_message.encode())
+
+server_response = s.recv(1024).decode()
+print(f"\nReceived from server: {server_response}")
 print(s.recv(1024))
 s.close()
--- Server.py
+++ Server.py
@@ -1,12 +1,19 @@
 import socket
 s = socket.socket()
 print("Socket created")
-port = 56988
+port = 56788
 s.bind(('', port))
+print(f"\nsocket binded to port : {port}")
 s.listen(5)
+print("\nServer is listening for incoming connections")
 while True:
     c, addr = s.accept()
     print(addr)
-    message = "Thank you for connecting"
+    message = "\nThank you for connecting"
+    c_message = c.recv(1024).decode()
+    print(f"\nReceived from client: {c_message}")
+
+    response_message = "\nMessage received by the server."
+    c.send(response_message.encode())
     c.send(message.encode())
     c.close()
